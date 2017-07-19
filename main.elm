bfa280266bb62e9aef05e3bd98ee7b5272bda275
Jordan Eldredge <jeldredge@fb.com>
2017-07-19 10:14:22 -0700

    Small refactor/cleanup

--- main.elm
+++ main.elm
@@ -3,8 +3,7 @@ module Main exposing (..)
 import Http
 import Html exposing (Html, div, text, ul, li, h1, table, tbody, tr, td, thead, th, a)
 import Html.Attributes exposing (href)
-import Json.Decode exposing (int, string, float, list, nullable, bool, decodeString, field, map7, map6, Decoder)
-import Json.Decode exposing (..)
+import Json.Decode exposing (string, list, bool, decodeString, field, map7, maybe, Decoder)
 import Debug exposing (log)
 import Date
 import Task exposing (perform)
@@ -13,46 +12,85 @@ import Task exposing (perform)
 main : Program Never Model Msg
 main =
     Html.program
-        { init = ( model, Cmd.batch [ getPerformances, now ] )
+        { init = init
         , view = view
         , update = update
         , subscriptions = subscriptions
         }
 
 
+getPerformances : Cmd Msg
+getPerformances =
+    let
+        url =
+            "https://raw.githubusercontent.com/captbaritone/singer-website/master/performances.json"
+    in
+        Http.send GotPerformances (Http.get url (list decodeShow))
+
+
+getDate : Cmd Msg
+getDate =
+    Date.now |> Task.perform SetDate
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
-
-
 -- MODEL
 
-
-type alias ShowRecord =
-    { character : String
-    , title : String
-    , company : String
-    , start_date : String
-    , end_date : String
-    , ticket_url : Maybe String
-    , sung_in_translation : Bool
-    }
-
-
-type alias Resume =
-    List ShowRecord
-
-
+-- Full application state
 type alias Model =
     { resume : Resume
     , renderDate : Maybe Date.Date
     }
 
+-- A list of shows
+type alias Resume =
+    List Show
 
-decodeShow : Decoder ShowRecord
+-- Show
+type alias Show =
+    { character : String
+    , title : String
+    , company : String
+    , startDate : String
+    , endDate : String
+    , ticketUrl : Maybe String
+    , sungInTranslation : Bool
+    }
+
+init = ( 
+    { resume = []
+    , renderDate = Nothing
+    }, Cmd.batch [ getPerformances, getDate ] )
+
+
+-- UPDATE
+
+type Msg
+    = GotPerformances (Result Http.Error Resume)
+    | SetDate Date.Date
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotPerformances (Ok resume) ->
+            ( { model | resume = resume }, Cmd.none )
+
+        GotPerformances (Err error) ->
+            -- TODO: Surface some kind of error?
+            ( model, Cmd.none )
+
+        SetDate date ->
+            ( { model | renderDate = Just date }, Cmd.none )
+
+
+
+
+decodeShow : Decoder Show
 decodeShow =
-    map7 ShowRecord
+    map7 Show
         (field "character" string)
         (field "title" string)
         (field "company" string)
@@ -62,23 +100,18 @@ decodeShow =
         (field "sung_in_translation" bool)
 
 
-decodeResume : Decoder Resume
-decodeResume =
-    list decodeShow
-
 compareDates : Date.Date -> Date.Date -> Order
-compareDates a b =
-    Basics.compare (Date.toTime a) (Date.toTime b)
+compareDates a b = Basics.compare (Date.toTime a) (Date.toTime b)
 
 
-showIsInTheFuture : Date.Date -> { a | end_date : String } -> Bool
+showIsInTheFuture : Date.Date -> Show -> Bool
 showIsInTheFuture now show =
-    case Date.fromString (show.end_date) of
+    case Date.fromString show.endDate of
         Err _ ->
             False
 
         Ok date ->
-            case log "cpm" (compareDates now date) of
+            case compareDates now date of
                 LT ->
                     False
 
@@ -89,21 +122,8 @@ showIsInTheFuture now show =
                     True
 
 
-futureResume : ( Date.Date, Resume ) -> Resume
-futureResume ( now, resume ) =
-    let
-        filterFuture =
-            List.filter (showIsInTheFuture now)
-    in
-        filterFuture resume
-
-
-model : Model
-model =
-    { resume = []
-    , renderDate = Nothing
-    }
-
+futureResume : Date.Date -> Resume -> Resume
+futureResume now = List.filter (showIsInTheFuture now)
 
 extractYear : String -> String
 extractYear date =
@@ -115,19 +135,34 @@ extractYear date =
             toString (Date.year d)
 
 
-upcomingShowRowView : ShowRecord -> Html Msg
-upcomingShowRowView show =
+-- VIEW
+
+-- The entire HTML page
+view : Model -> Html Msg
+view model =
+    case model.renderDate of
+        Just renderDate ->
+            div []
+                [ h1 [] [text "Upcomming"]
+                , viewUpcomingShows (futureResume renderDate model.resume)
+                ]
+
+        Nothing ->
+            div [] []
+
+viewUpcomingShowRow : Show -> Html Msg
+viewUpcomingShowRow show =
     let
-        { character, title, company, start_date, ticket_url } =
+        { character, title, company, startDate, ticketUrl } =
             show
     in
         tr []
             [ td [] [ text character ]
             , td [] [ text title ]
             , td [] [ text company ]
-            , td [] [ text (extractYear start_date) ]
+            , td [] [ text (extractYear startDate) ]
             , td []
-                [ case ticket_url of
+                [ case ticketUrl of
                     Nothing ->
                         -- TODO: Find a way to have no node here
                         a [] []
@@ -137,8 +172,8 @@ upcomingShowRowView show =
                 ]
             ]
 
-upcomingView : Resume -> Html Msg
-upcomingView resume =
+viewUpcomingShows : Resume -> Html Msg
+viewUpcomingShows resume =
     table []
         [ thead []
             [ th [] [ text "Character" ]
@@ -148,59 +183,8 @@ upcomingView resume =
             , th [] [] -- Tickets
             ]
         , tbody [] <|
-            List.map upcomingShowRowView <|
+            List.map viewUpcomingShowRow <|
                 resume
         ]
 
 
--- UPDATE
-type Msg = GotPerformances (Result Http.Error Resume)
-    | SetDate Date.Date
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        GotPerformances (Ok resume) ->
-            ( { model | resume = resume }, Cmd.none )
-
-        GotPerformances (Err error) ->
-            ( model, Cmd.none )
-
-        SetDate date ->
-            ( { model | renderDate = Just date }, Cmd.none )
-
-
-
--- VIEW
-view : Model -> Html Msg
-view model =
-    case model.renderDate of
-        Just renderDate ->
-            div []
-                [ h1 [] [text "Upcomming"]
-                , upcomingView (futureResume ( renderDate, model.resume ))
-                ]
-
-        Nothing ->
-            div [] []
-
-
-send : msg -> Cmd msg
-send msg =
-    Task.succeed msg
-        |> Task.perform identity
-
-
-getPerformances : Cmd Msg
-getPerformances =
-    let
-        url =
-            "https://raw.githubusercontent.com/captbaritone/singer-website/master/performances.json"
-    in
-        Http.send GotPerformances (Http.get url decodeResume)
-
-
-now : Cmd Msg
-now =
-    Date.now |> Task.perform SetDate
